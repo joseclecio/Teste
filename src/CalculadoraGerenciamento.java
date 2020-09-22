47f5a791d8f2c8bbc99af4ba5b71caa2c42212a5
Joseclecio <joseclecio.conceicao96@academico.ifs.edu.br>
2020-09-21 22:10:55 -0300

    Versão 1.2

--- src/CalculadoraGerenciamento.java
+++ src/CalculadoraGerenciamento.java
@@ -2,7 +2,12 @@ public class CalculadoraGerenciamento {
     private float value, keep;
     private int toDo;
 
-    void CalculadoraGerenciamento(){
+    void CalculadoraGerenciamento(){ // construtor
         value = keep=0;
+    }
+
+    void Operacao(int op, String tela){
+        keep = Float.parseFloat(tela);
+        toDo = op;
     }
 }
