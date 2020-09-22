380ffd25f7ec121e844e38937f601c92a91c7b66
Joseclecio <joseclecio.conceicao96@academico.ifs.edu.br>
2020-09-21 22:23:35 -0300

    Versão 1.1

---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Principal {
+    public static void main(String[] args) {
+        JFrame frame = new Calculadora();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
