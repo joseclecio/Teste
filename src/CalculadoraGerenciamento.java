84497e5cd0bdc581f9bc145c561818176ec018f4
Joseclecio <joseclecio.conceicao96@academico.ifs.edu.br>
2020-09-21 22:13:00 -0300

    Versão 1.3

--- src/CalculadoraGerenciamento.java
+++ src/CalculadoraGerenciamento.java
@@ -2,12 +2,24 @@ public class CalculadoraGerenciamento {
     private float value, keep;
     private int toDo;
 
-    void CalculadoraGerenciamento(){ // construtor
+
+    public void CalculadoraGerenciamento(){ // construtor
         value = keep=0;
     }
 
-    void Operacao(int op, String tela){
+    void Operacao(int op, String tela){ //operação da calculadora para exibir na tela
         keep = Float.parseFloat(tela);
         toDo = op;
+    }
+
+    float Display(String tela){
+        value = Float.parseFloat(tela);
+        switch(toDo){
+            case 0: return keep+value;
+            case 1: return keep-value;
+            case 2: return keep*value;
+            case 3: return keep/value;
+        }
+        return 0;
     }
 }
